1a6e720035f7b0dce5a40827baa9b5506d62017e
Ethan Franks <j.ethan.franks@gmail.com>
2025-01-07 15:36:44 -0600

    updated styling

--- src/app.tsx
+++ src/app.tsx
@@ -7,12 +7,12 @@ import gmailLogo from "./assets/gmail.png";
 export function App() {
   return (
     <>
-      <header className="w-full flex flex-row items-center justify-between bg-gradient-to-br from-[#b3cee8] to-[#617472] p-5 h-28 md:h-44">
+      <header className="w-full flex flex-row items-center justify-between bg-gradient-to-br from-[#313d46] to-[#597999] p-5 h-28 md:h-44">
         <section className="flex flex-col items-start justify-center">
-          <h1 className="text-black text-4xl md:text-5xl font-thin">
+          <h1 className="text-white text-4xl md:text-5xl font-thin md:tracking-widest">
             Ethan Franks
           </h1>
-          <p className="text-black text-xl md:text-2xl font-thin">
+          <p className="text-white text-xl md:text-2xl font-thin md:tracking-widest">
             Software Engineer
           </p>
         </section>
@@ -20,8 +20,8 @@ export function App() {
           <img src={ethan} className="h-44 md:h-64" />
         </div>
       </header>
-      <main className="flex flex-col items-center justify-center py-5 px-5 min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-256px)]"></main>
-      <footer className="flex flex-row items-center justify-end p-5 gap-4">
+      <main className="flex flex-col items-center justify-center pt-5 px-5 min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-256px)]"></main>
+      <section className="fixed bottom-0 right-0 w-[200px] flex flex-row gap-5 p-5 m:p-10 bg-black/70 rounded-tl-3xl">
         <a
           href="https://github.com/ethanfranks"
           target="_blank"
@@ -39,7 +39,7 @@ export function App() {
         >
           <img src={linkedinLogo} className="h-10 -pr-1" />
         </a>
-      </footer>
+      </section>
     </>
   );
 }
