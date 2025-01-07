1e8819abd85bf44efa925283338a6b7831beed02
Ethan Franks <j.ethan.franks@gmail.com>
2025-01-07 16:44:33 -0600

    nav bar & styling updates

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import gmailLogo from "./assets/gmail.png";
 export function App() {
   return (
     <>
-      <header className="w-full flex flex-row items-center justify-between bg-gradient-to-br from-[#313d46] to-[#597999] p-5 h-28 md:h-44">
+      <header className="w-full flex flex-row items-center justify-between bg-gradient-to-br from-[#313d46] to-[#597999] p-5 md:p-10 h-28 md:h-44">
         <section className="flex flex-col items-start justify-center">
           <h1 className="text-white text-4xl md:text-5xl font-thin md:tracking-widest">
             Ethan Franks
@@ -16,12 +16,26 @@ export function App() {
             Software Engineer
           </p>
         </section>
-        <div className="rounded-full overflow-hidden absolute top-2 right-2 md:top-5 md:right-5 shadow-2xl">
-          <img src={ethan} className="h-44 md:h-64" />
+        <div className="h-36 w-36 md:h-56 md:w-56 rounded-full overflow-hidden object-cover absolute top-3 right-3 md:top-5 md:right-10 shadow-2xl">
+          <img src={ethan} />
         </div>
       </header>
-      <main className="flex flex-col items-center justify-center pt-5 px-5 min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-256px)]"></main>
-      <section className="fixed bottom-0 right-0 w-[200px] flex flex-row gap-5 p-5 m:p-10 bg-black/70 rounded-tl-3xl">
+      <nav className="w-fit flex flex-row items-center bg-black/70 rounded-br-3xl">
+        <a
+          href="https://portfolio.ethanfranks.com"
+          className="flex items-center justify-center text-center text-white text-lg md:text-xl font-thin w-20 h-10 md:h-16 md:w-48 hover:bg-black/80 hover:font-extralight hover:shadow-xl transition-all"
+        >
+          <p>Portfolio</p>
+        </a>
+        <a
+          href="https://music.ethanfranks.com"
+          className="flex items-center justify-center text-center text-white text-lg md:text-xl font-thin w-20 h-10 md:h-16 md:w-48 hover:bg-black/80 hover:font-extralight hover:shadow-xl transition-all rounded-br-3xl"
+        >
+          <p>Music</p>
+        </a>
+      </nav>
+      <main className="flex flex-col items-center justify-center py-5 px-5 md:px-10 min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-256px)]"></main>
+      <section className="fixed bottom-0 right-0 flex flex-row gap-5 md:gap-10 p-5 md:px-10 bg-black/70 rounded-tl-3xl">
         <a
           href="https://github.com/ethanfranks"
           target="_blank"
