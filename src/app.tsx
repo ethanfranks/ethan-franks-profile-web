cb522ced032b728b01d8069b1b8ceff196cb4922
Ethan Franks <j.ethan.franks@gmail.com>
2025-01-07 13:13:34 -0600

    responsive page layout: header, main, footer with social links

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,45 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import "./app.css";
+import ethan from "./assets/ethan.jpg";
+import gitHubLogo from "./assets/github.png";
+import linkedinLogo from "./assets/linkedin.png";
+import gmailLogo from "./assets/gmail.png";
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
+      <header className="w-full flex flex-row items-center justify-between bg-gradient-to-br from-[#b3cee8] to-[#617472] p-5 h-28 md:h-44">
+        <section className="flex flex-col items-start justify-center">
+          <h1 className="text-black text-4xl md:text-5xl font-thin">
+            Ethan Franks
+          </h1>
+          <p className="text-black text-xl md:text-2xl font-thin">
+            Software Engineer
+          </p>
+        </section>
+        <div className="rounded-full overflow-hidden absolute top-2 right-2 md:top-5 md:right-5 shadow-2xl">
+          <img src={ethan} className="h-44 md:h-64" />
+        </div>
+      </header>
+      <main className="flex flex-col items-center justify-center py-5 px-5 min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-256px)]"></main>
+      <footer className="flex flex-row items-center justify-end p-5 gap-4">
         <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
+          href="https://github.com/ethanfranks"
           target="_blank"
+          rel="noreferrer"
         >
-          create-preact
+          <img src={gitHubLogo} className="h-10" />
         </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+        <a href="mailto:j.ethan.franks@gmail.com">
+          <img src={gmailLogo} className="h-10" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jethanfranks/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedinLogo} className="h-10 -pr-1" />
+        </a>
+      </footer>
     </>
-  )
+  );
 }
